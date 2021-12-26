7c79529dbd65af36642bff8477fee636b9050d88
Nəzrin <isbndyrva@gmail.com>
2021-12-26 20:46:16 +0400

    Update MateStore project

--- js/app.js
+++ js/app.js
@@ -1,50 +1,18 @@
-const productList = document.querySelector(".product-list");
+let productList = document.querySelector(".product-list");
 
 function loadJson() {
   fetch("data/products.json")
     .then((responce) => responce.json())
     .then((data) => {
+      let categories = document.querySelector(".filters");
       let item = "";
-      data.forEach((product) => {
-        item += `
-          <div class="product-item">
-            <div class="product-img-box">
-              <img
-                src="${product.imgSrc}"
-                alt="product image"
-                class="product-img"
-              />
-              <div class="content-overlay">
-                <div class="overlay-link">
-                  <div class="modal-dialog">
-                    <div class="modal-content">
-                      <div class="modal-img">
-                        <img src="${product.imgSrc}" />
-                      </div>
-                      <div class="modal-body">
-                        <h1>${product.name}</h1>
-                        <h2>${product.price}</h2>
-                        <p>${product.detail}</p>
-                        <p>Imported: NO</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="overlay-info">
-                  <p class="overlay-content-p">Design | Branding</p>
-                  <h5 class="text-center">Creative Web Design</h5>
-                </div>
-              </div>
-            </div>
-            <hr />
-            <div class="product-content d-flex justify-content-around">
-              <p class="product-price">${product.price}</p>
-              <button type="button" class="btn-add-cart mb-3 bg-transparent">
-                + Cart
-              </button>
-            </div>
-          </div>
-        `;
+      data.forEach((element) => {
+        if (element.filter_name == "All categories") {
+          element.products.forEach((element) => {
+            item += productView(element);
+          });
+        }
+        categories.innerHTML += categoryView(element);
       });
       productList.innerHTML = item;
     })
@@ -53,3 +21,72 @@ function loadJson() {
     });
 }
 loadJson();
+
+function categoryView(category) {
+  return `
+  <button class="filter-option" onclick="categoryFilter(${category.filter_id})">${category.filter_name}</button>
+  `;
+}
+
+function productView(product) {
+  return `
+  <div class="col-3 product-item" data="${product.id}">
+     <div class="product-img-box">
+       <img src="${product.imgSrc}" alt="product image" class="product-img" />
+       <div class="overlay">
+         <a class="overlay-link" href="product.html?id=${product.id}">
+           <img src="images/arrow.png" alt="arrow" class="arrow" />
+         </a>
+         <div class="overlay-info">
+           <p class="overlay-content-p">Design | Branding</p>
+           <h2 class="text-center">Creative Web Design</h2>
+         </div>
+       </div>
+       <p class="num-box"></p>
+     </div>
+     <hr />
+     <div class="product-content">
+       <p class="product-price">${product.price}</p>
+       <button type="button" class="btn btn-add-cart"><span class="add-btn">+</span>&nbsp;Cart</button>
+     </div>
+ </div>
+  `;
+}
+
+function categoryFilter(id) {
+  fetch("data/products.json")
+    .then((responce) => responce.json())
+    .then((data) => {
+      let products = document.querySelector(".products");
+      products.innerHTML = "";
+      for (let category of data) {
+        if (category.filter_id == id) {
+          category.products.forEach((element) => {
+            products.innerHTML += productView(element);
+          });
+        }
+      }
+    })
+    .catch((error) => {
+      alert(error);
+    });
+}
+
+// loader
+function removeLoader() {
+  $("#loadingDiv").fadeOut(500, () => {
+    $("#loadingDiv").remove();
+  });
+}
+
+$(window).on("load", () => {
+  setTimeout(removeLoader, 2000);
+
+  $("body").css(
+    "overflow-y",
+    "hidden",
+    setTimeout(() => {
+      $("body").css("overflow-y", "visible");
+    }, 2000)
+  );
+});
---
+++ js/product.js
@@ -0,0 +1,54 @@
+const productDetail = document.querySelector(".product-detail");
+
+function loadJson() {
+  fetch("data/products.json")
+    .then((responce) => responce.json())
+    .then((data) => {
+      let id = new URLSearchParams(window.location.search).get("id");
+      for (let filter of data) {
+        for (let product of filter.products) {
+          if (product.id == id) {
+            productDetail.innerHTML += `
+            <div class="row product-detail-item" data="${product.id}">
+            <div class="col-6 product-detail-img-box">
+              <img
+                src="${product.imgSrc}"
+                alt="product image"
+                class="product-detail-img"
+              />
+            </div>
+            <div class="col-6 product-detail-desc-box">
+              <p class="name">${product.name}</p>
+              <p class="price">${product.price}</p>
+              <p class="desc">${product.detail}</p>
+              <p class="subdesc">Imported: NO</p>
+              <hr />
+              <div class="like-icon">
+                <a href="javascript:void(0);" class="icon facebook">
+                  <i class="fa fa-facebook"></i>
+                  Like
+                </a>
+                <a href="javascript:void(0);" class="icon twitter">
+                  <i class="fa fa-twitter"></i>
+                  Tweet
+                </a>
+                <a href="javascript:void(0);" class="icon pinterest">
+                  <i class="fa fa-pinterest"></i>
+                  Save
+                </a>
+                <a href="javascript:void(0);" class="icon google">
+                  <i class="fa fa-google-plus"></i>
+                  Share
+                </a>
+              </div>
+            </div>
+          </div>`;
+          }
+        }
+      }
+    })
+    .catch((error) => {
+      alert(error);
+    });
+}
+loadJson();
